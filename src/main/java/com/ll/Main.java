0aa8775466874dda2975fc960bc7cf09add9189f
harriet221 <potter19@dongguk.edu>
2023-11-06 09:29:03 +0900

    1단계: 종료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,31 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new App().run();
+    }
+}
+
+class App {
+    Scanner sc;
+    App() {
+        sc = new Scanner(System.in);
+    }
+    void run() {
+        System.out.println("== 명언 앱 ==");
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            switch(cmd) {
+                case "종료":
+                    return;
+                default:
+                    System.out.println("유효하지 않은 명령입니다.");
+                    break;
+            }
+        }
     }
 }
