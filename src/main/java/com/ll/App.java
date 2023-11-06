b8fbbf9d9475e517c0f43c6f1622c15f6f66bf69
harriet221 <potter19@dongguk.edu>
2023-11-06 10:11:06 +0900

    5단계: 목록

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -17,15 +17,18 @@ public class App {
 
     void run() {
         System.out.println("== 명언 앱 ==");
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            switch(cmd) {
+            switch (cmd) {
                 case "종료":
                     return;
                 case "등록":
                     actionWrite();
+                    break;
+                case "목록":
+                    actionShow();
                     break;
                 default:
                     System.out.println("유효하지 않은 명령입니다.");
@@ -47,5 +50,14 @@ public class App {
         Quotation quote = new Quotation(id, content, author);
         quotationList.add(quote);
         System.out.printf("%d번 명령이 등록되었습니다.\n", id);
+    }
+
+    void actionShow() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for (int i = quotationList.size() - 1; i >= 0; i--) {
+            Quotation quote = quotationList.get(i);
+            System.out.printf("%d / %s / %s", quote.getId(), quote.getAuthor(), quote.getContent());
+        }
     }
 }
