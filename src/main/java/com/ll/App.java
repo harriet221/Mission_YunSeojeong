55e3bcc4d4868278aea38042bdc32e74b8457cb6
harriet221 <potter19@dongguk.edu>
2023-11-06 09:43:30 +0900

    2-4단계: 등록

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class App {
     Scanner sc;
+    int lastId;
+
     App() {
         sc = new Scanner(System.in);
+        lastId = 0;
     }
+
     void run() {
         System.out.println("== 명언 앱 ==");
         while(true) {
@@ -16,10 +20,26 @@ public class App {
             switch(cmd) {
                 case "종료":
                     return;
+                case "등록":
+                    actionWrite();
+                    break;
                 default:
                     System.out.println("유효하지 않은 명령입니다.");
                     break;
             }
         }
+    }
+
+    void actionWrite() {
+        System.out.print("명언 : ");
+        String content = sc.nextLine();
+
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
+        lastId++;
+        int id = lastId;
+
+        System.out.printf("%d번 명령이 등록되었습니다.\n", id);
     }
 }
