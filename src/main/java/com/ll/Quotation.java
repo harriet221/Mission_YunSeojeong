36bb1b33b353a03dbbdb65c8726cae17ba78de71
harriet221 <potter19@dongguk.edu>
2023-11-06 10:08:36 +0900

    getter & setter

--- src/main/java/com/ll/Quotation.java
+++ src/main/java/com/ll/Quotation.java
@@ -10,4 +10,24 @@ public class Quotation {
         this.content = content;
         this.author = author;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
